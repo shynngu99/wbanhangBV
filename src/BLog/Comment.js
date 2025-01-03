cdc5c6e64919d8ac38502b524c916d22d112fcc8
t2701 <73290014+shynngu99@users.noreply.github.com>
2025-01-03 15:58:37 +0700

    comment

--- src/BLog/Comment.js
+++ src/BLog/Comment.js
@@ -1,6 +1,28 @@
 export default function Comment() {
+    let checkLogin = localStorage.getItem('checkLogin')
+    if (checkLogin) {
+        console.log("CMT thành công");
+
+    } else {
+        console.log("bạn chưa đăng nhập");
+
+    }
     return (
-        <div>
-        </div >
+        <div class="replay-box">
+            <div class="row">
+                <div class="col-sm-12"><grammarly-extension data-grammarly-shadow-root="true" class="dnXmp" style="position: absolute; top: 0px; left: 0px; pointer-events: none;">
+                </grammarly-extension><grammarly-extension data-grammarly-shadow-root="true" class="dnXmp" style="position: absolute; top: 0px; left: 0px; pointer-events: none;">
+                    </grammarly-extension><h2>Leave a replay</h2>
+                    <div class="text-area">
+                        <div class="blank-arrow">
+                            <label>Your Name</label>
+                        </div>
+                        <span>*</span>
+                        <textarea name="message" rows="11" spellcheck="false"></textarea>
+                        <a class="btn btn-primary" href="">post comment</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
